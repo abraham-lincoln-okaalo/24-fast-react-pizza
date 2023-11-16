0afe2282cd5ad4b0e6bfc9e740ba8f06299891ee
Abraham <abrahamokaalo@gmail.com>
2023-11-16 15:32:53 +0000

    Advanced redux features

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     'inline-block text-sm rounded-full bg-yellow-400  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
 
   const styles = {
+    round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
     secondary:
@@ -16,10 +17,16 @@ function Button({ children, disabled, to, type }) {
         {children}
       </Link>
     );
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
   return (
-    <div disabled={disabled} className={styles[type]}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
-    </div>
+    </button>
   );
 }
 
